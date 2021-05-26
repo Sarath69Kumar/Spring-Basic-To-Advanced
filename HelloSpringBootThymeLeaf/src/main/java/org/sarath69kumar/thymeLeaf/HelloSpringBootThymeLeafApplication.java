91f167d6a11c036426182823234128aac694869b
PiscesSarath69 <59145492+Sarath69Kumar@users.noreply.github.com>
2021-05-26 20:49:58 +0530

    SpringBootThymeLeaf

---
+++ HelloSpringBootThymeLeaf/src/main/java/org/sarath69kumar/thymeLeaf/HelloSpringBootThymeLeafApplication.java
@@ -0,0 +1,13 @@
+package org.sarath69kumar.thymeLeaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringBootThymeLeafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringBootThymeLeafApplication.class, args);
+	}
+
+}
---
+++ HelloSpringBootThymeLeaf/src/test/java/org/sarath69kumar/thymeLeaf/HelloSpringBootThymeLeafApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sarath69kumar.thymeLeaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringBootThymeLeafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
